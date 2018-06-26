01114f3406779de12b1b04419d4aff6d59374935
meloncatty <k.handel66@gmail.com>
2018-06-25 22:11:20 -0600

    Add assignColors func Func updates color container backgrounds Func updates each container with appropriate rgb color text

--- public/src/index.js
+++ public/src/index.js
@@ -1,12 +1,23 @@
 var generatePalette = document.querySelector('.generate-palette-submit')
+var colorContainers = document.querySelectorAll('.palette-color')
 
 
-function generateColors(e) {
-  e.preventDefault()
+function generateColors() {
   var colorList = []
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 3; i++) {
     var randomRGB = Math.ceil(Math.random() * 256)
     colorList.push(randomRGB)
   }
   return colorList
+}
+
+generatePalette.addEventListener('click', assignColors)
+
+function assignColors(e) {
+  e.preventDefault()
+  colorContainers.forEach(container => {
+    var bgColor = generateColors()
+    container.style.background = 'rgb(' + bgColor[0] + ',' + bgColor[1] + ',' + bgColor[2] + ')'
+    container.innerText = 'rgb(' + bgColor[0] + ',' + bgColor[1] + ',' + bgColor[2] + ')'
+  })
 }
