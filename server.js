04642cfdac4c7ad5c382dee53016edfc64eddee0
meloncatty <k.handel66@gmail.com>
2018-06-25 20:27:40 -0600

    setup Express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Palette Picker'
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
