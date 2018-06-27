d3928b80cf53a00ab5c51872fc371e2b5343ea6d
meloncatty <k.handel66@gmail.com>
2018-06-26 23:49:55 -0600

    Add func to handle project creation Add func to add project to select menu upon creation

--- public/src/index.js
+++ public/src/index.js
@@ -1,9 +1,11 @@
 var generatePalette = document.querySelector('.generate-palette-submit')
 var colorContainers = document.querySelectorAll('.palette-color')
-
+var projectSubmit = document.querySelector('.add-project')
+var paletteSubmit = document.querySelector('.submit-new-palette')
 
 function generateColors() {
   var colorList = []
+
   for (var i = 0; i < 3; i++) {
     var randomRGB = Math.ceil(Math.random() * 256)
     colorList.push(randomRGB)
@@ -11,13 +13,39 @@ function generateColors() {
   return colorList
 }
 
-generatePalette.addEventListener('click', assignColors)
 
+generatePalette.addEventListener('click', assignColors)
+assignColors()
+
+//need case for no dupe colors
 function assignColors(e) {
-  e.preventDefault()
+
+  if (e) e.preventDefault()
+
   colorContainers.forEach(container => {
     var bgColor = generateColors()
     container.style.background = 'rgb(' + bgColor[0] + ',' + bgColor[1] + ',' + bgColor[2] + ')'
     container.innerText = 'rgb(' + bgColor[0] + ',' + bgColor[1] + ',' + bgColor[2] + ')'
   })
 }
+
+//endpoint to link proj + pal
+
+paletteSubmit.addEventListener('click', savePalette)
+
+function createProject(e) {
+  
+  e.preventDefault()
+
+  var projectName = document.querySelector('.project-name').value
+  addProjectToSelect(projectName)
+  projectName = ''
+}
+
+function addProjectToSelect(project) {
+  var selectEl = document.querySelector('.select-project')
+  selectEl.options.add(new Option(project))
+  document.querySelector('.project-name').value = ''
+}
+
+projectSubmit.addEventListener('click', createProject)
