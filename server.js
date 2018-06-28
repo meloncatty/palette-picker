d7fb25b23c14cdd6f21311d34fc8dd84b25d8d0b
Krista Handel <k.handel66@gmail.com>
2018-06-27 22:05:42 -0600

    Initialize server and database connection

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
 
 app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Palette Picker'
+
+app.get('/api/v1/projects', (req, res) => {
+  database('projects').select()
+    .then(projects => {
+      res.status(200).json(projects)
+    })
+    .catch((err) => {
+      response.status(500).json({ err })
+    })
+})
+
+app.post('/api/v1/projects', (req, res) => {
+  const { project } = req.body
+
+  database('projects').insert(project, 'id')
+    .then(projectId => {
+      res.status(201).json({projectId: projectId[0]})
+    })
+    .catch(err =>{
+      res.status(500).json({ err })
+    })
+})
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+  console.log(`Palette Picker is running on port ${app.get('port')}.`)
 })
